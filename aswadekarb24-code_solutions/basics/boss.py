01fb338735e54d5bb2a6723972ff75790467f6e6
Once-1296 <awwab1234@AwwabWadekar123.localdomain>
2026-03-04 04:57:42 +0000

    validation

--- aswadekarb24-code_solutions/basics/boss.py
+++ aswadekarb24-code_solutions/basics/boss.py
@@ -1,47 +1,49 @@
 choice = 'y'
 
-while choice =='y' : # make 'Y' valid too
+while choice =='y' or choice == 'Y' : # make 'Y' valid too
     try:
         # typecast the below 2 to a list
         
-        numbers =input("Enter the input numbers separated by spaces: ")
-        operators = input("Enter operators between them: ")
+        numbers =list(map(int,input("Enter the input numbers separated by spaces: ").split()))
+        operators = input("Enter operators between them: ").split()
 
         # check length matching
 
-        if len(numbers) != len(operators): # this seems odd... u might say it's ... off by one
+        if len(numbers) != len(operators) + 1: # this seems odd... u might say it's ... off by one
             print("What u doin fam ? ") # replace wiht better message :)
             continue
         
-        flag = False # huh this seems inverted
-        for i in range(len(numbers)): # indexing range fix
-            a, b, op = numbers[i-1], numbers[i], operators[i]
+        flag = True # huh this seems inverted
+        for i in range(len(operators)): # indexing range fix
+            a, b, op = numbers[i], numbers[i+1], operators[i]
+            #print(f"Calculating: {a} {op} {b}") # print the current operation being calculated
             # correct the ops
             match op:
                 case '+':
                     c = a + b
                 case '-':
-                    c = a * b
-                case '*':
-                    c = a / b
-                case '/':
-                    c = a % b
-                case '%':
                     c = a - b
+                case '*':
+                    c = a * b
+                case '/':
+                    c = a / b
+                case '%':
+                    c = a % b
                 case '//':
-                    c = a ** b
-                case '**':
                     c = a // b
+                case '**':
+                    c = a ** b
                 case _:
-                    flag = True
+                    flag = False
             if not flag:
                 print("Invalid ops vro")
                 break
 
-            numbers[i-1] = c
+            numbers[i+1] = c
+            #print(f"Intermediate result: {c}") # print intermediate result after each operation
         if not flag:
             continue
-        print(f"Output: numbers[-1]")
+        print(f"Output: {numbers[-1]}")
     except Exception:
         print(f"Exception: ...") # print exception
     finally:
