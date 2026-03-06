0474592e8a3f5fa64666a8445c1f1f74d92bfd5a
v1haan24 <vihaanjain2411@gmail.com>
2026-03-07 01:38:35 +0530

    Fix: completed basics and exercise

--- v1haan24_solutions/test_playground/basics/boss.py
+++ v1haan24_solutions/test_playground/basics/boss.py
@@ -1,22 +1,20 @@
 choice = 'y'
 
-while choice.lower() =='y' : # make 'Y' valid too
+while choice =='y' or choice=='Y' : # make 'Y' valid too
     try:
         # typecast the below 2 to a list
         
-        numbers =list(input("Enter the input numbers separated by spaces: ").split(" "))
-        numbers=[int(x) for x in numbers]
-        operators = list(input("Enter operators between them: ").split(" "))
+        numbers = list(map(float, input("Enter the input numbers separated by spaces: ").split()))
+        operators = input("Enter operators between them: ").split()
 
         # check length matching
-        print(type(numbers[0]))
+
         if len(numbers)-1 != len(operators): # this seems odd... u might say it's ... off by one
-            print("invalid syntax") # replace wiht better message :)
+            print("Not a valid input !") # replace wiht better message :)
             continue
         
         flag = True # huh this seems inverted
         for i in range(1,len(numbers)): # indexing range fix
-            #print(i)
             a, b, op = numbers[i-1], numbers[i], operators[i-1]
             # correct the ops
             match op:
@@ -43,12 +41,27 @@ while choice.lower() =='y' : # make 'Y' valid too
             numbers[i] = c
         if not flag:
             continue
-        print(f"Output:",numbers[-1])
-    except Exception as err:
-        print(f"Exception: ",err) # print exception
+        print(f"Output: {numbers[-1]}")
+    except Exception as exp:
+        print(f"Exception: {exp}") # print exception
     finally:
         choice = input("Do you want to continue? [y/n] : ") # always ask before ending
 
 # can you make the code shorter and with improved answer? 
 # like handling any basic arithmetic equation (that may have brackets too) ?
 # u might wanna find a special function in python
+
+#using eval()
+choice = 'y'
+
+while choice.lower() == 'y':
+    try:
+        expr = input("Enter an arithmetic expression: ")
+        result = eval(expr)  
+        print("Output:", result)
+
+    except Exception as exp:
+        print("Exception:", exp)
+
+    finally:
+        choice = input("Do you want to continue? [y/n]: ")
